8e27464c9bc22d599b7947977c10f1b2a18ca2ab
siddheshwarkhandare <235892541+siddheshwarkhandare@users.noreply.github.com>
2026-09-04 17:20:42 +0530

    hello page

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get('/')
-def home():
-    return ("hello")
+
+@app.get("/")
+async def read_root(request: Request):
+  return templates.TemplateResponse(request, "base.html", {"title": "Home"})
